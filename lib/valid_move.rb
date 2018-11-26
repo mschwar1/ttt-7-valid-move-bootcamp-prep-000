698c41aa7dab9ff2a66a72ad3dfd6d61977e2601
Mai Schwartz <mai.schwartz@gmail.com>
2018-11-26 21:27:36 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 
 def valid_move?(board, index)
   valid = false
-  taken = position_taken(board, index)
+  taken = position_taken?(board, index)
   if (index.between?(0,8) && taken == false)
     valid = true
   end
